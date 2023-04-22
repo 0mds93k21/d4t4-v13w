dc3cac75b927de975fdf909ce4dcbdac7cec9cd8
Myprofilee <andrep13.id@gmail.com>
2023-04-22 11:17:04 +0700

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -650,3 +650,33 @@ function toggleFullscreen(elem) {
 document.getElementById('btnFullscreen').addEventListener('click', function() {
     toggleFullscreen();
 });
+
+
+
+function toggleFullscreen(elem) {
+    elem = elem || document.documentElement;
+    if(!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+        if(elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if(elem.msRequestFullscreen) {
+            elem.msRequestFullscreen();
+        } else if(elem.mozRequestFullScreen) {
+            elem.mozRequestFullScreen();
+        } else if(elem.webkitRequestFullscreen) {
+            elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if(document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+document.getElementById('btnFullscreenMobile').addEventListener('click', function() {
+    toggleFullscreen();
+});
--- assets/plugins/firebase/home/myzuu.js
+++ assets/plugins/firebase/home/myzuu.js
@@ -1,39 +1,39 @@
 var config = {
-    apiKey: "AIzaSyBcMrJywvq9IZfBz5FvfcEBQlEUur5o6GM",
-    authDomain: "myzuu-help.firebaseapp.com",
-    databaseURL: "https://myzuu-help-default-rtdb.firebaseio.com",
-    projectId: "myzuu-help",
-    storageBucket: "myzuu-help.appspot.com",
-    messagingSenderId: "997562794816",
-    appId: "1:997562794816:web:d319134b4f850421a930a5",
-    measurementId: "G-THWEXQXCNZ"
-  };
-  
-  firebase.initializeApp(config);
-  
-  // Create a variable to reference the database.
-  var database = firebase.database();
-  
-  var connectionsRef = database.ref("/view aplikasi");
-  var connectedRef = database.ref(".info/connected");
-  
-  // When the client's connection state changes...
-  connectedRef.on("value", function(snap) {
-  
-    // If they are connected..
-    if (snap.val()) {
-  
-      // Add user to the connections list.
-      var con = connectionsRef.push(true);
-      // Remove user from the connection list when they disconnect.
-      con.onDisconnect().remove();
-    }
-  });
-  
-  // When first loaded or when the connections list changes...
-  connectionsRef.on("value", function(snap) {
-  
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#watchers-home").text(snap.numChildren());
-  });
+  apiKey: "AIzaSyCEliugQ19EDDcFKQnJCK7xUWtqBEWCnQo",
+  authDomain: "myzuu-view.firebaseapp.com",
+  databaseURL: "https://myzuu-view-default-rtdb.firebaseio.com",
+  projectId: "myzuu-view",
+  storageBucket: "myzuu-view.appspot.com",
+  messagingSenderId: "79139842519",
+  appId: "1:79139842519:web:0a77efc456a6fc9bf50392",
+  measurementId: "G-5P5Y0N9T9Y"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+var connectionsRef = database.ref("/view aplikasi");
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function (snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function (snap) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#watchers-home").text(snap.numChildren());
+});
